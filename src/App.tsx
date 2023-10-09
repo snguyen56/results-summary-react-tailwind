c822642caeb168f401689572be4ff3fdbbd311cd
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-10-09 18:34:49 -0400

    add HTML structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 function App() {
   return (
     <main>
-      Your Result
-      <span>76</span>
-      of 100 Great You scored higher than 65% of the people who have taken these
-      tests. Summary Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72
-      / 100 Continue
+      <h1>Your Result</h1>
+      <p>
+        <span>76</span>
+        of 100
+      </p>
+      <p>Great</p>
+      <p>
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+      <h2>Summary</h2>
+      <div>
+        Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 / 100
+      </div>
+      <button>Continue</button>
       <div className="attribution">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
