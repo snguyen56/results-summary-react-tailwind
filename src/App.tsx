8f26e3a2cde459c98ab54aa010abb3d09ff30c11
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2023-10-10 18:57:37 -0400

    add grid structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 function App() {
   return (
-    <main>
-      <h1>Your Result</h1>
-      <p>
-        <span>76</span>
-        of 100
-      </p>
-      <p>Great</p>
-      <p>
-        You scored higher than 65% of the people who have taken these tests.
-      </p>
-      <h2>Summary</h2>
-      <div>
-        Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 / 100
+    <main className="md:grid md:place-items-center h-screen">
+      <div className="md:grid grid-cols-2">
+        <div>
+          <h1>Your Result</h1>
+          <p>
+            <span>76</span>
+            of 100
+          </p>
+          <p>Great</p>
+          <p>
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
+        </div>
+        <div>
+          <h2>Summary</h2>
+          <div>
+            Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 / 100
+          </div>
+          <button>Continue</button>
+        </div>
       </div>
-      <button>Continue</button>
-      <div className="text-center text-[11px]">
+      {/* <div className="text-center text-[11px]">
         Challenge by{" "}
         <a
           href="https://www.frontendmentor.io?ref=challenge"
@@ -29,7 +35,7 @@ function App() {
           Your Name Here
         </a>
         .
-      </div>
+      </div> */}
     </main>
   );
 }
